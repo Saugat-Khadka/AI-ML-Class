700d399b80f11f31316d02ac4c5714b72210f761
Saugat <ksaugat140@gmail.com>
2026-07-07 12:31:58 +0545

    Day 8

--- Day 8/main.py
+++ Day 8/main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from day-8!")
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.write("Hello, World!")
 
-if __name__ == "__main__":
-    main()
+st.write("Here's my first attempt at using data to create tables:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+st.button("Click me")
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+#HTML Language also works in streamlit. You can use HTML tags to format your text. For example, you can use the <h1> tag to create a heading.
+st.write("<h1> Heading 1 </h1>", unsafe_allow_html=True)
+
+# markdown language as alternative to html
+st.markdown('# Heading 1')
+
+# forms
+
+num = st.number_input("Enter a number")
+
+if st.button("Calculate"):
+    st.write(f"The number is {num}")
+    st.write(f"The square of the number is {num**2}")
---
+++ Day 8/titanic_ui.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.write("# Titanic Survivor Prediction")
+
+st.write("* If you were on titanic, would you have survived? Let's find out! *")
+
+# Passengers Age
+age = st.number_input("Enter your age", min_value=0, max_value=100)
+
+# Passengers Gender
+gender = st.selectbox("Select your gender", ("Male", "Female"))
+
+# Passengers Class
+class_options = st.selectbox("Select your class", ("1", "2", "3"))
+
+# Passengers Sibblings/Spouses
+sibs = st.number_input("Enter number of siblings/spouses aboard", min_value=0, max_value=10)
+
+# Passengers Parents/Children
+parch = st.number_input("Enter number of parents/children aboard", min_value=0, max_value=10)
+
+# Button to predict survival
+st.button("Predict")
+
